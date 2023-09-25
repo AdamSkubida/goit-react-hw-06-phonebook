a9531532d0a605c65a5936c932a3a85dfae8e4db
AdamSkubida <adasiow.s@gmail.com>
2023-09-26 00:01:17 +0200

    bugFix/localStorage | App.jsx, reducer.js

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -2,12 +2,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import { addContact, deleteContact, filterContact } from './actions';
 import { Notify } from 'notiflix';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = JSON.parse(localStorage.getItem('contacts')) || [];
 const filterInitialState = '';
 
 export const contanctsReducer = createReducer(contactsInitialState, {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { ContactForm } from './ContactForm/index';
 import { Filter } from './Filter/index';
 import { ContactList } from './ContactList/index';
@@ -9,6 +9,18 @@ import { setContacts } from 'Redux/actions';
 export const App = () => {
   const dispatch = useDispatch();
 
+  const isLoaded = useRef(false);
+
+  const contacts = useSelector(getAllContacts);
+
+  useEffect(() => {
+    if (contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    } else {
+      localStorage.removeItem('contacts');
+    }
+  }, [contacts]);
+
   useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
     console.log(storedContacts);
@@ -16,13 +28,13 @@ export const App = () => {
     if (storedContacts) {
       dispatch(setContacts(JSON.parse(storedContacts)));
     }
+
+    isLoaded.current = true;
+
+    return () => {
+      isLoaded.current = false;
+    };
   }, [dispatch]);
-
-  const contacts = useSelector(getAllContacts);
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div>
       <ContactForm />
